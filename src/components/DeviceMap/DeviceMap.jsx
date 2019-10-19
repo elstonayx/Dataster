fb90c213981e9cf40c29877031bc1db60accc3d3
Elston Aw <elston.ayx@u.nus.edu>
2019-10-19 12:42:24 -0400

    Add text layer for map (#3)

--- src/components/DeviceMap/DeviceMap.jsx
+++ src/components/DeviceMap/DeviceMap.jsx
@@ -1,15 +1,40 @@
-import React from "react";
-import { Map, TileLayer } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import request from "request-promise-native";
+import { Map, TileLayer, LayersControl, GeoJSON } from "react-leaflet";
 import PropTypes from "prop-types";
 
+const { BaseLayer } = LayersControl;
+
 export const DeviceMap = props => {
+  const [geoJSONData, setGeoJSONData] = useState();
+
+  useEffect(() => {
+    request({
+      method: "GET",
+      uri:
+        "https://pmmpublisher.pps.eosdis.nasa.gov/products/s3/Global/global_landslide_nowcast/2019/288/global_landslide_nowcast_20191015.geojson"
+    }).then(data => {
+      const parsedData = JSON.parse(data);
+      setGeoJSONData(parsedData);
+    }, console.log);
+
+    return () => {};
+  });
   return (
     <div style={{ width: "inherit", height: "inherit" }}>
       <Map center={props.position} zoom={10}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
+        <LayersControl>
+          <BaseLayer checked name="ERSI Satellite">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </BaseLayer>
+          <BaseLayer checked name="names">
+            <TileLayer url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png" />
+          </BaseLayer>
+        </LayersControl>
+        {geoJSONData && <GeoJSON data={geoJSONData} color="red" />}
       </Map>
     </div>
   );
