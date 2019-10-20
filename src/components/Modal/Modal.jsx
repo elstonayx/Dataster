bb311bd9d457f7f4b1317dcd082e82490f037450
elstonayx <elston.ayx@u.nus.edu>
2019-10-20 08:55:32 -0400

    Stop message fetching on component re-render

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
-import React ,{ useState, useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
 import request from "request-promise-native";
 import Button from '@material-ui/core/Button';
 
@@ -15,23 +15,22 @@ function getModalStyle() {
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
+    transform: `translate(-${top}%, -${left}%)`
   };
 }
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
+    padding: theme.spacing(2, 4, 3)
+  }
 }));
 
 export function SimpleModal(props) {
-  
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
@@ -46,20 +45,17 @@ export function SimpleModal(props) {
     setOpen(false);
   };
 
-  
   useEffect(() => {
     request({
       method: "GET",
-      uri:
-        "https://dataster-c6fa8.firebaseio.com/Country/Messages.json"
+      uri: "https://dataster-c6fa8.firebaseio.com/Country/Messages.json"
     }).then(data => {
-      console.log(data)
+      console.log(data);
       const parsedData = JSON.parse(data);
       setDistrictJSONData(parsedData);
     }, console.log);
-    return () => { };
-
-  });
+    return () => {};
+  }, []);
 
   return (
     <div>
@@ -73,6 +69,7 @@ export function SimpleModal(props) {
         onClose={handleClose}
       >
         <div style={modalStyle} className={classes.paper}>
+<<<<<<< HEAD
           <h2> Priority: {props.priority}: {props.type} </h2>
           <p><strong>Location:</strong> {props.location}</p>
           <p> <strong>Latitude:</strong> {props.latitude} , Longitude: {props.longitude}</p>
@@ -80,6 +77,18 @@ export function SimpleModal(props) {
             <strong>Message</strong>: {props.description}
           </p>
           
+=======
+          <h1>
+            {" "}
+            {props.priority}: {props.type}{" "}
+          </h1>
+          <h2>{props.location}</h2>
+          <h3>
+            {" "}
+            Latitude: {props.latitude} , Longitude: {props.longitude}
+          </h3>
+          <p id="simple-modal-description">{props.description}</p>
+>>>>>>> Stop message fetching on component re-render
         </div>
       </Modal>
     </div>
@@ -87,6 +96,6 @@ export function SimpleModal(props) {
 }
 
 SimpleModal.defaultProps = {
-  type: 'test',
-  description: 'test2'
-}
+  type: "test",
+  description: "test2"
+};
